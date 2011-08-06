a0f9c684b1848559ae40cddd57d2c5df7cad909b
Nick Reid <nickreid@nickreid.com>
2011-08-06 15:21:09 -0700

    adding basic templates/routes

--- jobapp.py
+++ jobapp.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flaskext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/nickreid/Documents/craigslist_maps/jobmaps/test.db'
 db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
