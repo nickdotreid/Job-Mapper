858b6fbc75368195960ac1d9abaef4479e267b97
Nick Reid <nickreid@nickreid.com>
2011-08-08 10:39:49 -0700

    added get jobs route

--- jobapp.py
+++ jobapp.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flaskext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/nickreid/Documents/craigslist_maps/jobmaps/test.db'
 db = SQLAlchemy(app)
 
+from models import *
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+	
+@app.route('/jobs')
+def get_jobs():
+	jobs = len(Post.query.all())
+	return jsonify(total = jobs)
