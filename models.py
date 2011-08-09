2bcc93236534dae16a812dd41b26430b8add2806
Nick Reid <nickreid@nickreid.com>
2011-08-08 18:36:13 -0700

    added region model

--- models.py
+++ models.py
@@ -18,30 +18,58 @@ class PostType(db.Model):
 	def __repr__(self):
 		return '<Type %r>' % self.short
 
+region_to_posts = db.Table('region_to_posts',
+	db.Column('region_id', db.Integer, db.ForeignKey('region.id')),
+	db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
+	)
+
+region_to_parent = db.Table('region_to_parent',
+	db.Column('region_id',db.Integer,db.ForeignKey('region.id')),
+	db.Column('parent_id',db.Integer,db.ForeignKey('region.id'))
+	)
+
+
+class Region(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	short = db.Column(db.String(80))
+	name = db.Column(db.String(200), nullable=True)
+	
+	posts = db.relationship('Post',secondary=region_to_posts,backref=db.backref('posts',lazy='dynamic'))
+	
+	parent_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+	children = db.relationship("Region",
+		lazy="joined",
+		join_depth=2)
+	
+	def __init__(self,short):
+		self.short = short
+	
+	def __repr__(self):
+		return '<Region %r>' % self.short
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	post_id = db.Column(db.String(120))
-	post_type_id = db.Column(db.Integer, db.ForeignKey('post_type.id'))
 
 	post_date = db.Column(db.DateTime)
 	remove_date = db.Column(db.DateTime, nullable=True)
 
 	title = db.Column(db.String(500))
 	link = db.Column(db.String(300))
-
-	source = db.Column(db.String(100))
-	city = db.Column(db.String(300))
-	neighborhood = db.Column(db.String(300))
-
-	def __init__(self, post_id, post_date=datetime.datetime.now(), title='test', link='http://sfbay.craigslist.org', source='test source', city='test city', neighborhood='test neighborhood'):
+	
+	post_type_id = db.Column(db.Integer, db.ForeignKey('post_type.id'))
+	
+	source = db.relationship('Region',uselist=False ,secondary=region_to_posts, backref=db.backref('source',lazy='dynamic'))
+	region = db.relationship('Region',uselist=False ,secondary=region_to_posts, backref=db.backref('region',lazy='dynamic'))
+	section = db.relationship('Region',uselist=False ,secondary=region_to_posts, backref=db.backref('section',lazy='dynamic'))
+	
+	def __init__(self, post_id, post_date=datetime.datetime.now(), title='test', link='http://sfbay.craigslist.org'):
 		self.post_id = post_id
 		
 		self.post_date = post_date
 		self.title = title
 		self.link = link
-		self.source = source
-		self.city = city
-		self.neighborhood = neighborhood
 
 	def __repr__(self):
 		return '<Post %r>' % self.post_id
+		
