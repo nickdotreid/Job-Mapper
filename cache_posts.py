69f438d4ed0a1e39df0eb121affac3da4f275a49
Nick Reid <nickreid@nickreid.com>
2011-12-18 19:03:48 -0800

    changed function completely

--- cache_posts.py
+++ cache_posts.py
@@ -1,17 +1,24 @@
 from models import *
-
-def count_posts(post_type,region):
-	posts = Post.query.filter_by(post_type=post_type).filter_by(post_type=post_type).all()
-	return len(posts)
 	
 types = PostType.query.all()
-regions = Region.query.all()
+regions = Region.query.filter_by(published=True).all()
 
 for region in regions:
-	print region.short
+	print "*****"
+	posts = region.posts
+	post_count = PostCount(len(posts),None,region.id,None)
+	db.session.add(post_count)
+	db.session.commit()
+	print str(post_type.name)+"--"+str(post_count.count)
+	count_types = {}
 	for post_type in types:
-		print post_type.name
-		#check for existing post_count & update or create new
-		post_count = PostCount(count_posts(post_type,region),post_type.id,region.id,None)
+		count_types[post_type.short] = 0
+	for post in posts:
+		if post.post_type:
+			count_types[post.post_type.short] += 1
+	for post_type in types:
+		count = count_types[post_type.short]
+		post_count = PostCount(count,post_type.id,region.id,None)
+		print str(post_type.name)+"--"+str(post_count.count)
 		db.session.add(post_count)
 		db.session.commit()
