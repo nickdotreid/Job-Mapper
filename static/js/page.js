ecc9e51c2778c2ce573edb121b92fb744a503198
Nick Reid <nickreid@nickreid.com>
2011-12-25 22:37:06 -0800

    made linear scale change to filtered items

--- static/js/page.js
+++ static/js/page.js
@@ -33,6 +33,7 @@ $(document).ready(function(){
 						'size':data['types'][type],
 					});
 				}
+				region.data("types",types);
 				d3.select(region[0]).append('div').attr("class","display").selectAll('div.type').data(types).enter().append('div').attr("class",function(d){
 					return "type "+d.short;
 				}).style("width","0px");
@@ -41,13 +42,24 @@ $(document).ready(function(){
 		});
 	}).delegate(".region","draw",function(event){
 		region = $(this);
-		var x = d3.scale.linear().domain([0, region.data("total")]).range([0, $(".display",region).width()]);
+		var x = d3.scale.linear().domain([0, d3.sum(region.data("types"),function(d){
+			if(!$("#"+d.short).attr("checked")){
+				return 0;
+			}
+			return d.size;
+		})]).range([0, $(".display",region).width()]);
 		d3.select(this).selectAll(".display .type").transition().duration(1500).style("width",function(d){
 			num = x(d.size)
 			if(num<2 || !$("#"+d.short).attr("checked")){
 				return "0px";
 			}
 			return (num-1)+'px';
+		}).style("border-right-width",function(d){
+			num = x(d.size)
+			if(num<2 || !$("#"+d.short).attr("checked")){
+				return "0px";
+			}
+			return "1px";
 		});
 	}).trigger({
 		type:'add_region',
