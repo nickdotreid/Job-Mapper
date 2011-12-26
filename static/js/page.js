07bf84624d04172e47b2f35af1377a82bf774fa5
Nick Reid <nickreid@nickreid.com>
2011-12-25 16:06:44 -0800

    added quick add regions

--- static/js/page.js
+++ static/js/page.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#content").bind("add_region",function(event){
 		$("#content #chart").append('<div class="region '+event.region+'" data-short="'+event.region+'"><h3 class="title">'+event.region+'</h3></div>');
-		$(".region").trigger("fetch");
+		$(".region:last").trigger("fetch");
 	}).delegate(".region","fetch",function(event){
 		region = $(this)
 		
@@ -34,13 +34,13 @@ $(document).ready(function(){
 					});
 				}
 				d3.select(region[0]).append('div').attr("class","display").selectAll('div.type').data(types).enter().append('div').attr("class","type").style("width","0px");
-				$(".region").trigger("draw");
+				$("#chart .region").trigger("draw");
 			}
 		});
 	}).delegate(".region","draw",function(event){
 		region = $(this);
 		var x = d3.scale.linear().domain([0, region.data("total")]).range([0, $(".display",region).width()]);
-		d3.selectAll(".region .display .type").transition().duration(1500).style("width",function(d){
+		d3.select(this).selectAll(".display .type").transition().duration(1500).style("width",function(d){
 			num = x(d.size)
 			if(num<2 || !$("#"+d.short).attr("checked")){
 				return "0px";
@@ -56,4 +56,11 @@ $(document).ready(function(){
 		$("#chart .region").trigger("draw");
 	});
 	
+	$("#content").delegate(".quick_add a","click",function(event){
+		event.preventDefault();
+		$("#content").trigger({
+			type:'add_region',
+			region:$(this).attr("href")
+		});
+	});
 })
